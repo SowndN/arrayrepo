721f00c5ead333645cc8c11d43be75208beefba6
SowndN <sowndharyavb2696@gmail.com>
2022-03-10 13:43:06 +0530

    dev1

--- src/org/singlearray/SingleArray.java
+++ src/org/singlearray/SingleArray.java
@@ -6,8 +6,9 @@ public static void main(String[] args) {
 	a[0]=9;
 	a[1]=12;
 	a[2]=14;
-	
+	System.out.println("hi github");
 	// find a particular array value
+	
 	
 	System.out.println("for GIT");
 	
